cf32ce554b67d727be2dec4e86cd5298e43e8c66
filipelamberti <filipelamberti@gmail.com>
2014-11-18 21:40:01 +0000

    Alterando as urls e a views

--- projetoTrabalho/urls.py
+++ projetoTrabalho/urls.py
@@ -1,12 +1,14 @@
+# -*- coding: utf-8 -*-
 from django.conf.urls import patterns, include, url
+from cadastro.views import *
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'projetoTrabalho.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = patterns(
+    'cadastro.views',
+    url(r'^$', 'home', name='home'),
+    url(r'^cadastro/$', Criar.as_view(), name='cadastro'),
+    url(r'^lista/$', Listar.as_view(), name='lista'),
     url(r'^admin/', include(admin.site.urls)),
 )
