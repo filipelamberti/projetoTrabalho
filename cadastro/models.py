d48cc68d0456206ffedf2b401ebc25db8a659e17
filipelamberti <filipelamberti@gmail.com>
2014-11-18 16:51:32 +0000

    Implementando a classe models

--- cadastro/models.py
+++ cadastro/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class inscricao (models.model):
+      nome = models.CharField(max_length=100)
+      cpf = models.CharField('CPF', max_length=11, unique=True)
+      idade = models.IntegerField()
+      email = models.EmailField()
+      telefone = models.CharField(max_length=20, blank=True)
+      criado_em = models.DateTimeField('criado em', auto_now_add =True)
+      
+      class Meta:
+              ordering = ['criado em']
+              verbose_name = (u'nome')
+              verbose_name_plural = (u'nome')
+              
+      def __unicode__(self):
+              return self.nome
